c22e1ccf977ff665255708ea9d7c336fce2a4e7e
jisoo0609 <jisu20000609@gmail.com>
2023-11-21 10:42:55 +0900

    Refactoring

---
+++ T231120/src/HelloJava.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        Scanner sc  = new Scanner(System.in);
+
+    }
+}
---
+++ T231120/src/Q2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Q2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("두개의 숫자를 입력하세요: ");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(b);
+		System.out.println(a);
+	}
+}
