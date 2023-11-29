691530ce4500be6b7afce20be3073b78a828ea44
jisoo0609 <jisu20000609@gmail.com>
2023-11-29 17:03:17 +0900

    231129 Set

---
+++ src/T231129/Collections/Pigeon.java
@@ -0,0 +1,15 @@
+package T231129.Collections;
+
+/*
+비둘기집 원리
+비둘기가 들어갈 수 있는 집이 10개인데
+비둘기가 11마리라면
+누군가는 한방에 두마리 이상이 들어간다.
+ */
+public class Pigeon {
+    private int age;
+
+    public Pigeon(int age) {
+        this.age = age;
+    }
+}
