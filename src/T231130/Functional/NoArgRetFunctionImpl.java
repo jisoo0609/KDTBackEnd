8f8f6a52e459c9079214ecb2faad459aeb6228b7
jisoo0609 <jisu20000609@gmail.com>
2023-11-30 15:44:06 +0900

    231130 Functional Interface

---
+++ src/T231130/Functional/NoArgRetFunctionImpl.java
@@ -0,0 +1,9 @@
+package T231130.Functional;
+
+// 인터페이스의 구현체
+public class NoArgRetFunctionImpl implements NoArgRetFunction {
+    @Override
+    public void noArgNoReturn() {
+        System.out.println("Traditional Implementation");
+    }
+}
---
+++ src/T231130/Functional/StrArgIntRetFunction.java
@@ -0,0 +1,5 @@
+package T231130.Functional;
+@FunctionalInterface
+public interface StrArgIntRetFunction {
+    int run(String s);
+}
