b53114081c41fdee780774ad41921bd45fe2752b
jisoo0609 <jisu20000609@gmail.com>
2023-12-08 15:04:15 +0900

    231208 Brute Force

--- src/Algorithm/BOJ/B1158.java
+++ src/Algorithm/BOJ/B1158.java
@@ -3,38 +3,54 @@ package Algorithm.BOJ;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class B1158 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        Queue<Integer> queue = new LinkedList<>();
+        // 정답을 담기 위한 리스트
+        // 담을 원소의 갯수를 알고 있다면 생성자 생성시 그 값을 전달해 배열을 미리 키울 수 있다
+        List<Integer> result = new ArrayList<>(n);
+        // 요세푸스 문제 정의대로 사람을 빼기 위한 큐
+        Queue<Integer> people = new LinkedList<>();
 
         // 큐에 1~n까지 숫자 넣음
         for (int i = 1; i <= n; i++) {
-            queue.offer(i);
+            people.offer(i);
         }
 
+        // 현재 몇번째 사람인지를 기록한 뒤
+        int round = 0;
+        // 큐가 빌때까지 반복
+        while(!people.isEmpty()) {
+            int person = people.poll();
+            // 이번 사람이 탈락할 차례인지 판단
+            if (round == k-1) {
+                // 맞으면 result로
+                result.add(person);
+                // 초기화
+                round = 0;
+            }
+            else {
+                // 아니면 다시 큐로
+                people.offer(person);
+                round++;
+            }
+        }
+
+        String resultString = result.toString();
+        // 부문문자열 만들기
+        resultString = resultString.substring(1, resultString.length()-1);
+        // System.out.println("<"+resultString+">");
+        StringBuilder sb = new StringBuilder();
         sb.append("<");
-
-        while (!queue.isEmpty()) {
-            for (int i = 0; i < k-1; i++) {
-                queue.offer(queue.poll());
-            }
-            if (queue.size() == 1) {
-                sb.append(queue.poll()).append(">");
-            } else {
-                sb.append(queue.poll()).append(",");
-            }
-        }
+        sb.append(resultString);
+        sb.append(">");
 
         System.out.println(sb);
     }
---
+++ src/T231208/Combination.java
@@ -0,0 +1,58 @@
+package T231208;
+
+import java.util.Arrays;
+
+public class Combination {
+    // 5개 중에서 3개를 선택하는 방ㅇ법들을 출력하는 메서드
+    // 5C3을 출력하는 메서드
+    public static void combSimple(int n) {
+        int first;
+        int second;
+        int third;
+        // i는 0 부터 n - 2까지 반복하고,
+        for (int i = 0; i < n - 2; i++) {
+            first = i;
+            // j는 i + 1 부터 n - 1까지 반복하고,
+            for (int j = first; j < n -1; j++) {
+                second = j;
+                // k는 j + 1 부터 n - 1까지 반복한다.
+                for (int k = 0; k < n; k++) {
+                    third = k;
+                    System.out.println(first + " "+ second + " "+ third);
+                }
+            }
+        }
+    }
+
+    // 재귀함수로 nCr 구하는 메서드
+    // n개 중에서 r개 고름
+    // int count: 지금까지 몇개의 원소를 골랐는지
+    // int next: 이번에 고를지 말지를 판단중인 숫자
+    // int[] comb: 조합 결과를 담을 배열
+    public static void combRecurHelper(int n, int r, int count, int next, int[] comb) {
+        // r개 골라야 하는데 다 골랐다
+        if (count == r) {
+            System.out.println(Arrays.toString(comb));
+        }
+        // 만약 내가 고르려고 고려할 숫자가 범위를 벗어나려 한다면
+        else if (next == n) {
+            return;
+        }else {
+            // 만약 내가 이번에 next를 골랐으면
+            comb[count] = next;
+            // count 번째 원소를 골랐으니, count + 1번째 원소를 고르러 감
+            combRecurHelper(n, r, count + 1, next + 1, comb);
+
+            // count 번째 원소를 고르지 않고 다음 원소를 확인한다
+            combRecurHelper(n, r, count, next + 1, comb);
+        }
+    }
+
+    public static void combRecur(int n, int r) {
+        combRecurHelper(n, r, 0, 0, new int[r]);
+    }
+
+    public static void main(String[] args) {
+        combSimple(5);
+    }
+}
